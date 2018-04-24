2ec7a047c057d69a03217d99528755b97fe3160b
Shrimant More <smshrimant@gmail.com>
2018-04-25 01:31:51 +0530

    Update run.php

--- run.php
+++ run.php
@@ -1,25 +1,22 @@
 <html>
-<body>
-<head>
-<title>
-run
-</title>
-</head>
+ <body>
+  <head>
+   <title>
+     run
+   </title>
+  </head>
 
-<form method="post">
+   <form method="post">
 
-<input type="submit" value="GO" name="GO">
-</form>
-</body>
+    <input type="submit" value="GO" name="GO">
+   </form>
+ </body>
 </html>
 
 <?php
-	
 	if(isset($_POST['GO']))
 	{
 		shell_exec("python /var/www/html/lab/mkdir.py");
 		echo"success";
 	}
-
-
 ?>
